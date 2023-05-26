dc64255964e741d2ca270bfa413737cc0fcbc045
DmKeller <dmit.keller@gmail.com>
2023-05-26 12:07:52 +0300

    Initial Commit

---
+++ RealmTaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmTaskApp
+//
+//  Created by Келлер Дмитрий on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
